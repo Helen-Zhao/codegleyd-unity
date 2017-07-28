2a67447558db456f6106e1505c8ebf65674e67e1
Helen Zhao <helen4251996@yahoo.co.nz>
2017-07-29 11:45:49 +1200

    Remove library files

--- Assets/Instantiation.cs
+++ Assets/Instantiation.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using Random = System.Random;
+using SimpleJson;
 
 namespace Assets
 {
@@ -17,24 +21,12 @@ namespace Assets
         public GameObject Tile6;
         readonly Random _rand = new Random();
 
+        private string _userID;
+        private string _authToken;
+
         void Start()
         {
-            for (int x = 0; x < 15; x = x+3)
-            {
-                for (int z = 0; z < 15; z = z + 3)
-                {
-                    GameObject obj = GetRandomTile();
-                    float angleToRotate = GetRandomAngle();
-                    Quaternion q = Quaternion.AngleAxis(angleToRotate, Vector3.up);
-
-                    // Transform to account for movement
-                    int newX = angleToRotate == 270 || angleToRotate == 180 ? x - 3 : x;
-                    int newZ = angleToRotate == 90f || angleToRotate == 180f ? z - 3 : z;
-
-                    // Create obj
-                    Instantiate(obj, new Vector3(newX, 0, newZ), q);
-                }
-            }
+            
         }
 
         private GameObject GetRandomTile()
@@ -65,9 +57,123 @@ namespace Assets
             return angle * 90f;
         }
 
+        void StoreUserID(string userID, string authToken)
+        {
+            this._userID = userID;
+            this._authToken = authToken;
+            var userData = GetUserData();
+
+            if (userData.SimValues == null)
+            {
+                
+            }
+
+            /**
+             * Example user data
+             * 
+             */
+
+            Console.WriteLine(userData);
+
+            for (int x = 0; x < 15; x = x + 3)
+            {
+                for (int z = 0; z < 15; z = z + 3)
+                {
+                    GameObject obj = GetRandomTile();
+                    float angleToRotate = GetRandomAngle();
+                    Quaternion q = Quaternion.AngleAxis(angleToRotate, Vector3.up);
+
+                    // Transform to account for movement
+                    int newX = angleToRotate == 270 || angleToRotate == 180 ? x - 3 : x;
+                    int newZ = angleToRotate == 90f || angleToRotate == 180f ? z - 3 : z;
+
+                    // Create obj
+                    Instantiate(obj, new Vector3(newX, 0, newZ), q);
+                }
+            }
+        }
+        
+        private UserData GetUserData()
+        {
+            IEnumerable userData = SendHttpRequest();
+            Console.WriteLine(userData);
+            Debug.Log(userData);
+            return null;
+        }
+
+        private IEnumerable SendHttpRequest()
+        {
+            //http request
+            UnityWebRequest www = UnityWebRequest.Get("http://localhost:5000/api/userdata/" + _userID);
+            www.SetRequestHeader("Authorization", "Bearer " + _authToken);
+            www.SetRequestHeader("Content-Type", "application/json");
+            yield return www.Send();
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+                JsonUtility.FromJson<UserData>(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                //                byte[] results = www.downloadHandler.data;
+            }
+        }
+
         // Update is called once per frame
         void Update()
         {
         }
+    }
+
+
+    // Models
+
+    public class UserData
+    {
+        public int ID { get; set; }
+        public string UserId { get; set; }
+        public int Gold { get; set; }
+        public int GoldSpent { get; set; }
+        public string SerializeStorage { get; set; }
+        public virtual ICollection<SimulationValue> SimValues { get; set; }
+
+        public UserData() { }
+
+        public UserData(string userId)
+        {
+            this.Gold = 1;
+            this.GoldSpent = 0;
+            this.SerializeStorage = "";
+            this.SimValues = new List<SimulationValue>();
+            this.UserId = userId;
+        }
+    }
+    public class SimulationValue
+    {
+        public int ID { get; set; }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public string Rotation { get; set; }
+        public Tile Tile { get; set; }
+
+        public int UserDataID { get; set; }
+
+        public SimulationValue() { }
+    }
+
+    public class Tile
+    {
+        public int ID { get; set; }
+        public int Code { get; set; }
+        public int Cost { get; set; }
+        public int Value { get; set; }
+        public string Type { get; set; }
+        public string Label { get; set; }
+
+        public Tile() { }
     }
 }
